425b8f4868a2ff6b854856278d3ffa0a6973645d
appoliana <polinapikkio@gmail.com>
2019-03-25 21:33:06 +0300

    new homework

---
+++ MyNUnit/MyNUnit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+// найти сборки, запустить тесты в сборке
+// информацию вывести на экран
+
+// получить информацию о сборке с помощью рефлексии
+// найти методы, помеченные анотацией Test, Expected и Ignore
+// параллельно запустить тесты
+
+// обработать тест
+// получить результат работы теста
+
+// написать тесты на систему тестирования
+
+
+
+// ?? что такое методы before и after
