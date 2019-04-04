24b909241aecde7c4c431b07dd635fc44501a868
appoliana <polinapikkio@gmail.com>
2019-04-04 16:52:16 +0300

    with tests

---
+++ ProjectThreadPool/ProjectThreadPool/MyThreadPool/MyTask.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace MyThreadPool
+{
+    public class MyTask<TResult> : IMyTask<TResult>
+    {
+        protected TResult result;
+        protected Func<TResult> job;
+
+        protected Exception innerException;
+        protected MyThreadPool threadPool; //ссылка на наш пул
+        protected object lockObject;
+
+        //для обмена сообщениями между потоками
+        private AutoResetEvent getResultsEvent;
+
+        // нужно для ContinueWith
+        private ManualResetEvent taskAddedEvent;
+        private ConcurrentQueue<Action> queueJobs;
+        
+
+        public MyTask(MyThreadPool threadPool1, Func<TResult> job1)
+        {
+            job1 = job;
+            threadPool1 = threadPool;
+            lockObject = new object();
+            getResultsEvent = new AutoResetEvent(false);
+            taskAddedEvent = new ManualResetEvent(true);
+            queueJobs = new ConcurrentQueue<Action>();
+        }
+
+        public bool IsCompleted { get; private set; }
+
+        public TResult Result
+        {
+            get
+            {
+                getResultsEvent.WaitOne(); //свойство вернет результат только когда получит сигнал о том что есть результат
+
+                if (innerException == null)
+                {
+                    return result;
+                }
+
+                throw new AggregateException(innerException); //собирает со всех дочерних потоков исклбчения
+            }
+        }
+
+        public void DoJob()
+        {
+            try
+            {
+                result = job();
+            }
+
+            catch (Exception ex)
+            {
+                innerException = ex;
+            }
+
+            lock (lockObject)
+            {
+                getResultsEvent.Set();
+                IsCompleted = true;
+                FinishAndContinueJobs();
+            }
+        }
+
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> job)
+        {
+            // оболочка для функции, которая продолжает вычисление на основе полученного результата в том же пуле
+            var task = new MyTask<TNewResult>(threadPool, () => job(result));
+            lock (lockObject)
+            {
+                if (!IsCompleted) //если не закончена работа
+                {
+                    // помещаем новую задачу в наш пул на выполнение
+                    queueJobs.Enqueue(() => threadPool.AddTask<TNewResult>(task));
+                    return task;
+                }
+            }
+            return threadPool.AddTask(task); // если закончена, то сразу в пул
+        }
+
+        private void FinishAndContinueJobs()
+        {
+            if (queueJobs.Count == 0)
+            {
+                return;
+            }
+
+            if (innerException == null)
+            {
+                if (!threadPool.IsActive)
+                {
+                    queueJobs = null;
+                    return;
+                }
+
+                foreach (Action job in queueJobs)
+                {
+                    job(); //помещаем в пул на выполнение и задача начинает считаться
+                }
+
+                queueJobs = null;
+            }
+            else
+            {
+                throw new AggregateException(innerException); //место для всех исключений
+            }
+        }
+    }
+}
---
+++ ProjectThreadPool/ProjectThreadPool/MyThreadPool/MyThreadPool.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MyThreadPool
+{
+    public class MyThreadPool
+    { 
+        // ёмкость пула (общее кол-во)
+        protected int poolCapacity;
+        // счётчик оставновленных нитей
+        protected int stopedThreadsCounter;
+        // объект для блокировки элементов (защита от гонки данных)
+        protected object lockObject;
+        // спец объект для отмены выполнения нитей
+        protected CancellationTokenSource cts;
+
+        protected List<Thread> threadsList;
+
+        // очередь из методов на обработку в наших нитях
+        protected ConcurrentQueue<Action> jobsQueue;       
+
+        // сигнальные объекты
+        protected AutoResetEvent eventTaskAdded;
+        protected AutoResetEvent eventJobDone;
+
+        public bool IsActive
+        {
+            get => !cts.Token.IsCancellationRequested;
+        }
+
+        public MyThreadPool(int tasksQuantity)
+        {
+            poolCapacity = tasksQuantity;
+            stopedThreadsCounter = 0;
+            lockObject = new object();
+            cts = new CancellationTokenSource();
+            jobsQueue = new ConcurrentQueue<Action>();
+            eventTaskAdded = new AutoResetEvent(false);
+            eventJobDone = new AutoResetEvent(false);
+
+            StartPool();
+        }
+
+        protected void StartPool()
+        {
+            threadsList = new List<Thread>(poolCapacity);
+            for (var i = 0; i < poolCapacity; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    while (IsActive)
+                    {
+                        if (jobsQueue.TryDequeue(out Action doJob))
+                        {
+                            doJob();
+                        }
+                        else
+                        {
+                            eventTaskAdded.WaitOne();
+                        }
+                    }
+
+                    lock (lockObject)
+                    {
+                        stopedThreadsCounter++;
+                    }
+                    eventJobDone.Set();
+                });
+
+                threadsList.Add(thread);
+                thread.IsBackground = true;
+                thread.Start();
+            }
+        }
+
+        public IMyTask<TResult> AddJob<TResult>(Func<TResult> job)
+        {
+            var task = new MyTask<TResult>(this, job);
+            return AddTask<TResult>(task);
+        }
+
+        public IMyTask<TResult> AddTask<TResult>(MyTask<TResult> task)
+        {
+            if (!IsActive)
+            {
+                throw new MyThreadPoolCanceledException();
+            }
+
+            jobsQueue.Enqueue(task.DoJob);
+            eventTaskAdded.Set();
+
+            return task;
+        }
+
+        public void Shutdown()
+        {
+            cts.Cancel();
+            eventTaskAdded.Set();
+            while (true) // ждет, пока закончат выполнение все нити
+            {
+                eventJobDone.WaitOne();
+                eventTaskAdded.Set();
+
+                lock (lockObject)
+                {
+                    if (poolCapacity == stopedThreadsCounter)
+                    {
+                        break;
+                    }
+                }
+            }
+            jobsQueue = null;
+        }
+    }
+}
