a42269741802ba2baa491c6719920adc1a17e033
appoliana <polinapikkio@gmail.com>
2019-04-05 18:31:35 +0300

    хм я не понимаю что надо делать со thread

---
+++ PriorityQueue/PriorityQueue/MyQueue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    public class MyQueue<T>
+    {
+        /// <summary>
+        /// Лист, в котором будут храниться элементы.
+        /// </summary> 
+        List<QueueElement<T>> listQueue = new List<QueueElement<T>>();
+        private Object lockOn = new Object();
+        
+        class QueueElement<T>
+        {
+            public T Value { get; set; }
+            public int Priority { get; set; }
+        }
+
+        /// <summary>
+        /// Метод, который добавляет эдемент в очередь.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="priority"></param>
+        public void Enqueue(T value, int priority)
+        {
+            lock (listQueue)
+            {
+                QueueElement<T> newElement = new QueueElement<T>
+                {
+                    Value = value,
+                    Priority = priority
+                };
+                
+                if (IsEmpty())
+                {
+                    listQueue.Add(newElement);
+                }
+                
+                else
+                {
+                    int index = listQueue.FindIndex(x => (x.Priority >= priority));
+                    if (index == -1)
+                    {
+                        listQueue.Add(newElement);
+                    }
+                    else
+                    {
+                        listQueue.Insert(index, newElement);
+                    }
+                }
+                if (listQueue.Count == 1)
+                {
+                    Monitor.PulseAll(listQueue);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод, который удаляет элемент с наибольшим приоритетом из очереди.
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+            lock (listQueue)
+            {
+                // если очередь пуста, ждем
+                while (listQueue.Count() == 0)
+                {
+                    Monitor.Wait(listQueue);
+                }
+                T result = listQueue.ElementAt(listQueue.Count() - 1).Value;
+                listQueue.RemoveAt(listQueue.Count() - 1);
+                return result;
+            }
+        }
+
+        int Size()
+        {
+            return listQueue.Count();
+        }
+
+        bool IsEmpty()
+        {
+            return listQueue.Count() == 0;
+        }
+    }
+}
---
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    class Program
+    {
+        static void EnqueueThread(MyQueue<int> myq)
+        {
+            myq.Enqueue(1, 1);
+            myq.Enqueue(4, 4);
+            myq.Enqueue(3, 3);
+            myq.Enqueue(5, 4);
+            myq.Enqueue(2, 2);
+        }
+
+        public static void DequeueThread(MyQueue<int> myq)
+        {
+            Console.WriteLine(myq.Dequeue());
+            Console.WriteLine(myq.Dequeue());
+            Console.WriteLine(myq.Dequeue());
+            Console.WriteLine(myq.Dequeue());
+            Console.WriteLine(myq.Dequeue());
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            MyQueue<int> myq = new MyQueue<int>();
+
+            Thread t1 = new Thread(delegate () { EnqueueThread(myq); });
+            t1.Start();
+            Thread t2 = new Thread(delegate () { DequeueThread(myq); });
+            t2.Start();
+        }
+    }
+}
+
